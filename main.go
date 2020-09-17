9951455722927eb40b9cc96506fdea67b65ee8f5
taolx <taolx25@foxmail.com>
2020-09-17 13:53:46 +0800

    注释

--- main.go
+++ main.go
@@ -9,15 +9,12 @@ import (
 func main() {
 	fmt.Println("ChitChat", version(), "started at", config.Address)
 
-	// handle static assets
+	// handle static file
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir(config.Static))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
-	//
 	// all route patterns matched here
-	// route handler functions defined in other files
-	//
 
 	// index
 	mux.HandleFunc("/", index)
