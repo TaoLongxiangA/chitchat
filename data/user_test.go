a147c5b5ecba026d0e3e88356f060e0f176c0a51
taolx <taolx25@foxmail.com>
2020-09-22 09:44:41 +0800

    initial commit

--- data/user_test.go
+++ data/user_test.go
@@ -13,11 +13,11 @@ func Test_UserCreate(t *testing.T) {
 	if users[0].Id == 0 {
 		t.Errorf("No id or created_at in user")
 	}
-	u, err := UserByEmail(users[0].Email)
+	user, err := UserByEmail(users[0].Email)
 	if err != nil {
 		t.Error(err, "User not created.")
 	}
-	if users[0].Email != u.Email {
+	if users[0].Email != user.Email {
 		t.Errorf("User retrieved is not the same as the one created.")
 	}
 }
