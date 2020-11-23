c5e8125e35530ec038326ca7dd9564fdbd24a276
taolx0 <taolx25@foxmail.com>
2020-11-23 16:35:39 +0800

    modify decoder

--- utils.go
+++ utils.go
@@ -36,9 +36,7 @@ func loadConfig() {
 	if err != nil {
 		log.Fatalln("Cannot open config file", err)
 	}
-	decoder := json.NewDecoder(file)
-	config = Configuration{}
-	err = decoder.Decode(&config)
+	err = json.NewDecoder(file).Decode(&config)
 	if err != nil {
 		log.Fatalln("Cannot get configuration from file", err)
 	}
@@ -51,7 +49,7 @@ func errorMessage(writer http.ResponseWriter, request *http.Request, msg string)
 }
 
 // Checks if the user is logged in and has a session, if not err is not nil
-func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
+func session(_ http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
 	cookie, err := request.Cookie("_cookie")
 	if err == nil {
 		sess = data.Session{Uuid: cookie.Value}
@@ -85,20 +83,20 @@ func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...str
 }
 
 // for logging
-func info(args ...interface{}) {
-	logger.SetPrefix("INFO ")
-	logger.Println(args...)
-}
+//func info(args ...interface{}) {
+//	logger.SetPrefix("INFO ")
+//	logger.Println(args...)
+//}
 
 func danger(args ...interface{}) {
 	logger.SetPrefix("ERROR ")
 	logger.Println(args...)
 }
 
-func warning(args ...interface{}) {
-	logger.SetPrefix("WARNING ")
-	logger.Println(args...)
-}
+//func warning(args ...interface{}) {
+//	logger.SetPrefix("WARNING ")
+//	logger.Println(args...)
+//}
 
 // version
 func version() string {
